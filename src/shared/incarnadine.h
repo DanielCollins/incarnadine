cf14d110fe1e396ca42ad8071b53f54055b8309c
Daniel Collins <novi.pyro@gmail.com>
2010-03-18 17:48:09 +1100

    added incarnadine.h

---
+++ src/shared/incarnadine.h
@@ -0,0 +1,12 @@
+#ifndef SHARED_INCARNADINE_H
+#define SHARED_INCARNADINE_H
+
+
+#include <cml/cml.h>
+
+typedef cml::vector3f vector3;
+typedef cml::matrix44f_c matrix44;
+
+#include <cmath>
+
+#endif //SHARED_INCARNADINE_H
