48055338721f6d440b4483f1a2066550eb4833e7
Daniel Collins <novi.pyro@gmail.com>
2010-03-22 11:09:35 +1100

    clean ups to renderScene

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -97,15 +97,14 @@ void renderScene()
 {
 	glClearColor (0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glTranslatef(0.0f,0.0f,-6.0f);
 		
 	glBegin(GL_TRIANGLES);
-	  glColor3f(1.0f,0.0f,0.0f);
-	  glVertex3f(0.0f, 1.0f, 0.0f);
-	  glColor3f(0.0f,1.0f,0.0f);
-	  glVertex3f(-1.0f,-1.0f, 0.0f);
-	  glColor3f(0.0f,0.0f,1.0f);
-	  glVertex3f(1.0f,-1.0f, 0.0f);
+	  glColor3f(1.0,0.0,0.0);
+	  glVertex3f(0.0, 1.0, 0.0);
+	  glColor3f(0.0,1.0,0.0);
+	  glVertex3f(-1.0,-1.0, 0.0);
+	  glColor3f(0.0,0.0,1.0);
+	  glVertex3f(1.0,-1.0, 0.0);
     glEnd();
 }
 
