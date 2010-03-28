667dc39ad34307275ddd970d0f213bc1c85af6a0
Daniel Collins <novi.pyro@gmail.com>
2010-03-28 17:23:11 +1000

    use sdl_gl double buffering, don't require sdl blitting to be double buffered

--- src/engine/display.cpp
+++ src/engine/display.cpp
@@ -36,9 +36,20 @@ Display::~Display()
 
 bool Display::init()
 {
+	//require at least 5 bits per colour
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+	
+	//require at least 2 byte depth buffer
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+	
+	//require double buffering
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)	return false;	 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	surface = SDL_SetVideoMode(640, 480, 16, SDL_OPENGL | SDL_DOUBLEBUF | SDL_HWSURFACE);
+	surface = SDL_SetVideoMode(640, 480, 16, SDL_OPENGL | SDL_HWSURFACE);
 	return true;
 }
 
