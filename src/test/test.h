c3c8dfc2b45e224b550aee676cdd56bbfe4238a7
Daniel Collins <novi.pyro@gmail.com>
2010-04-20 22:54:46 +1000

    connect to exiting event

--- src/test/test.h
+++ src/test/test.h
@@ -4,11 +4,15 @@
 #include <stdlib.h>
 
 #include "incarnadine.h"
+#include "events.h"
 #include "scene.h"
 #include "render.h"
 #include "tools.h"
+#include "input.h"
 
 void runTest();
 void exitTestApp();
+
+void handleExit(Exiting);
 
 #endif //TEST_H
