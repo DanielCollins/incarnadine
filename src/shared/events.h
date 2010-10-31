08a9edbfa45aba7e66d3b015543025ddbf556591
PurplePanda <novi.pyro@gmail.com>
2010-10-31 14:41:30 +1100

    merged event stuff into a single file

--- src/shared/events.h
+++ src/shared/events.h
@@ -1,5 +1,5 @@
 /* 
-   Copyright (c) 2010 Daniel Collins, Daniel Flahive
+   Copyright (c) 2010 Daniel Collins
 
    Permission to use, copy, modify, and distribute this software for any
    purpose with or without fee is hereby granted, provided that the above
@@ -16,6 +16,133 @@
 
 #ifndef SHARED_EVENTS_H
 #define SHARED_EVENTS_H
-#include "signal.h"
-#include "slot.h"
+
+#include <vector>
+
+template <class EventType>
+class Signal;
+
+template <class EventType>
+class Slot
+{
+   private:
+
+      std::vector<Signal<EventType>*> signals;
+      void (*handler)(EventType);
+      bool enabled;
+
+
+   public:
+
+      Slot(void (*newhandler)(EventType))
+      {
+         handler = newhandler;
+         enabled = true;
+      }
+
+      ~Slot()
+      {
+      }
+
+      void connect(Signal<EventType>* signal)
+      {
+         typename std::vector<Signal<EventType>*>::const_iterator i;
+         for(i = signals.begin(); i != signals.end(); i++)
+            if(signal == *i)
+               return;
+         signals.push_back(signal);
+         signal->_acceptConnection(this);
+      }
+
+      void disconnect(Signal<EventType>* signal)
+      {
+         typename std::vector<Signal<EventType>*>::const_iterator i;
+         for(i = signals.begin(); i != signals.end(); i++)
+         {
+            if(signal == *i)
+            {
+               signal->_disconnect(this);
+               signals.erase(i);
+               return;
+            }
+         }
+      }
+
+      void _fire(EventType* e)
+      {
+         if(enabled) handler(*e);
+      }
+
+      bool isEnabled()
+      {
+         return enabled;
+      }
+
+      void setEnabled(bool newState)
+      {
+         enabled = newState;
+      }
+};
+
+template <class EventType>
+class Signal
+{
+   private:
+      std::vector<Slot<EventType>*> slots;
+      bool enabled;
+
+   public:
+
+      Signal()
+      {
+         enabled = true;
+      }
+
+      ~Signal()
+      {
+      }
+
+      void fire(EventType* e)
+      {
+         if(enabled)
+         {
+            typename std::vector<Slot<EventType>*>::const_iterator i;
+            for(i = slots.begin(); i != slots.end(); i++)
+               (*i)->_fire(e);
+         }
+      }
+
+      void _acceptConnection(Slot<EventType>* newSlot)
+      {
+         typename std::vector<Slot<EventType>*>::const_iterator i;
+         for(i = slots.begin(); i != slots.end(); i++)
+            if(newSlot == *i)
+               return;
+         slots.push_back(newSlot);
+      }
+
+      void _disconnect(Slot<EventType>* oldSlot)
+      {
+         typename std::vector<Slot<EventType>*>::const_iterator i;
+         for (i = slots.begin(); i != slots.end(); i++)
+         {
+            if(oldSlot == *i)
+            {
+               slots.erase(i);
+               return;
+            }
+         }
+      }
+
+      bool isEnabled()
+      {
+         return enabled;
+      }
+
+      void setEnabled(bool newState)
+      {
+         enabled = newState;
+      }
+};
+
 #endif //SHARED_EVENTS_H
