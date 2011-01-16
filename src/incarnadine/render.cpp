90cb26bbe66367b9a2fb78a36e1701971b2eca35
PurplePanda <novi.pyro@gmail.com>
2011-01-16 19:33:54 +1100

    fixed a code style issue in the render code

--- src/incarnadine/render.cpp
+++ src/incarnadine/render.cpp
@@ -30,7 +30,7 @@ RenderManager::RenderManager(Camera *newCamera, Scene *newScene, Window *newWind
    glEnable(GL_CULL_FACE);      
    glEnable(GL_BLEND); 
    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-   glClearColor (1.0, 1.0, 1.0, 0.0);
+   glClearColor(1.0, 1.0, 1.0, 0.0);
 }
 
 RenderManager::~RenderManager()
